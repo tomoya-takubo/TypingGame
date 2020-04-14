eaf58ad31be77333347bf8598b5aa71e7b6a0184
tomoya-takubo <tkb1003@gmail.com>
2020-04-14 21:20:08 +0900

    202004142115 コミット
    https://gametukurikata.com/program/typingsoft
    を参考に、・キー入力画面表示・正誤判定・問題出題・正解タイプ数を実装

---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//enumを使うために必要
+using System;
+
+public class GameDirector : MonoBehaviour
+{
+    public Text inputText;
+
+    void Update()
+    {
+        //DownKeyCheck();
+
+        GetInputString();
+    }
+
+
+    /***
+    void DownKeyCheck()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode code in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(code))
+                {
+                    //処理を書く
+                    //Debug.Log(code);
+                    inputText.text += code.ToString();
+                    break;
+                }
+            }
+        }
+    }
+    ***/
+
+    void GetInputString()
+    {
+        if (Input.anyKeyDown)
+        {
+            inputText.text += Input.inputString;
+        }
+    }
+
+}
---
+++ Assets/Scripts/QuestionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionController : MonoBehaviour
+{
+    public char chr = 'a';
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().text = chr.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
